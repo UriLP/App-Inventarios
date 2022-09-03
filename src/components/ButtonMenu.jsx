bf0a025dcd6e5495e8145ef18cf355fcc1ba7734
UriLP <dash9uri@gmail.com>
2022-09-03 14:00:45 -0500

    Botones con iconos

--- src/components/ButtonMenu.jsx
+++ src/components/ButtonMenu.jsx
@@ -1,43 +1,53 @@
 import React from "react"
 import { StyleSheet, Text, View } from "react-native"
 import StyledText from "./StyledText"
+import CircumIcon from "@klarr-agency/circum-icons-react";
 
 const ButtonMenu = ({ children }) => {
+
+  // const icon = iconos.box ? { children } == 'Sistema' : iconos.laptop
+
   return (
     <View style={ styles.botonMenu }>
-      <StyledText color='white'>{ children }</StyledText>
+      <StyledText style={ styles.card } color='white'>
+        {/* <CircumIcon style={ styles.icon }  color='white' name={ iconos.box }/> */}
+        
+        { children }
+      </StyledText>
     </View>
   )
 }
 
 export default ButtonMenu
 
+
+const iconos = {
+  laptop: 'laptop',
+  box: 'box_list'
+}
+
 const styles = StyleSheet.create({
-  container: {
-    backgroundColor: '#eee',
-    padding: 10,
-    height: '80%',
-    flexDirection: 'row',
-    marginHorizontal: 30,
-    marginVertical: 30,
-    display: "flex",
-    justifyContent: "center",
-    borderRadius: '5%'
-  },
   botonMenu: {
     backgroundColor: 'gray',
     marginHorizontal: 30,
     marginVertical: 30,
-    height: 100,
-    width: 100,
+    height: 150,
+    width: 200,
     display: "flex",
     justifyContent: "center",
     alignItems: "flex-end",
     paddingBottom: 5,
-    borderRadius: '5%',
+    borderRadius: 10,
     flexDirection: 'row',
   },
   text: {
     color: '#fff'
+  },
+  card: {
+    display: "flex",
+    flexDirection: "column",
+    padding: 10,
+    justifyContent: "center",
+    alignItems: "center"
   }
 })
