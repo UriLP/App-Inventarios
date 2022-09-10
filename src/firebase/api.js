cda4f59a8ee02adb33c35a411705157113337d8b
Daniel Ramírez <daniel.rmz.olv@gmail.com>
2022-09-10 14:53:58 -0500

    Se conecto firebase con la app y se hizo la primera funcion para la api

---
+++ src/firebase/api.js
@@ -0,0 +1,13 @@
+import { addDoc, collection, doc, DocumentSnapshot, getDocs, updateDoc } from "firebase/firestore";
+import { db } from "./config";
+
+const colMedi = collection(db, "medicinas");
+
+export const getAllMedicinas = async() => {
+    const respuesta = await getDocs(colMedi);
+    const allMedicinas = [];
+    respuesta.forEach( (medicina) => {
+        allMedicinas.push({...medicina.data(), id : medicina.id});
+    });
+    return allMedicinas;
+}
